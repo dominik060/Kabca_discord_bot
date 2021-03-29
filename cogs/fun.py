b4af67551ed8e7359b669f706160d4e23d210006
dominik060 <71775710+dominik060@users.noreply.github.com>
2021-03-29 11:55:35 +0200

    Add files via upload

--- cogs/fun.py
+++ cogs/fun.py
@@ -1,199 +1,64 @@
-import json
-import random
-import vtipek
-import certifi
+import re
+from urllib import parse, request
 import discord
 import requests
-import eightballer
-import urllib.request
-from io import BytesIO
-from bs4 import BeautifulSoup
-from unidecode import unidecode
 from discord.ext import commands
-from discord.ext.commands import has_permissions
+import random
+class fun(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-class Fun(commands.Cog):
-	def __init__(self,bot):
-		self.bot = bot
+    @commands.command(aliases=['8b'], pass_context=True)
+    async def _8ball(self, ctx, *, question):
+        responses = [
+            "It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes - definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."]
+        em = discord.Embed(color=discord.Color.grey())
+        em.add_field(name="Qustion: ", value=f"{str(question)}", inline=False)
+        em.add_field(name="Answer: ", value=f"{random.choice(str(responses))}", inline=False)
+        await ctx.channel.send(embed=em)
 
-	@commands.command(pass_context = True,no_pm=True,aliases=['clean','delete','smaz','ocista'])
-	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
-	@has_permissions(manage_messages=True)
-	async def purge(self,ctx, number):	
-		await ctx.channel.trigger_typing()
-		mgs = [] #Empty list to put all the messages in the log
-		number = int(number) #Converting the amount of messages to delete to an integer
-		if number > 100:
-			return await ctx.channel.send('V klidu s tou čistkou, limit je 100 zpráv')
-		if number!=100:
-			number+=1
-		async for x in ctx.channel.history(limit = number):
-			mgs.append(x)
-		await ctx.channel.delete_messages(mgs)
-		await ctx.channel.send(":put_litter_in_its_place: x"+str(number))
+    @commands.command()
+    async def cat(self, ctx):
+        response = requests.get('https://aws.random.cat/meow')
+        data = response.json()
+        await ctx.channel.send(data['file'])
 
+    @commands.command()
+    async def say(self, ctx, *args):
+        response = ""
+        for arg in args:
+            response = response + " " + arg
+        await ctx.channel.send(response)
 
-	@commands.command(pass_context=True)
-	@commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
-	async def kdojsem(self,ctx, member: discord.Member = None):
-		author = ctx.message.author
-		msg = "Jsi "+str(author)
-		await ctx.channel.send(msg)
-	
-	@commands.command(pass_context=True, aliases = ["8ball","magickakoule","choose"])
-	@commands.cooldown(rate=1, per=2, type=commands.BucketType.user)
-	async def eightball(self,ctx):
-		msg = eightballer.eightball_pick()
-		await ctx.channel.send(msg)
-	@commands.command(pass_context=True)
-	@commands.cooldown(rate=1, per=3, type=commands.BucketType.user)
-	async def vtip(self,ctx):
-		msg = vtipek.vtipek()
-		await ctx.channel.send("`"+msg+"`")
-	@commands.command(pass_context=True)
-	@commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
-	async def fakt(self,ctx):
-		url = "http://www.faktomat.cz/fakty/nahodne"
-		r = urllib.request.urlopen(url)
-		soup = BeautifulSoup(r,'html.parser')
-		result = soup.find("div", {"class":"lead"}).text
-		await ctx.channel.send(f"`{result}`")
-	@commands.command(pass_context=True,no_pm=True)
-	@commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
-	async def avatar(self,ctx,*,user:discord.Member = None):
-		if user is None:
-			user = ctx.message.author
-		await ctx.channel.send(f" **{user.name}**\n{user.avatar_url}")
-
-	@commands.command(pass_context = True,no_pm=True,aliases=["server"])
-	@commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
-	async def guild(self, ctx):
-		""" Check info about current guild """
-		if ctx.invoked_subcommand is None:
-			findbots = sum(1 for member in ctx.message.guild.members if member.bot)
-
-			embed = discord.Embed(colour = discord.Colour.blue())
-			embed.set_thumbnail(url=ctx.message.guild.icon_url)
-			embed.add_field(name="Název serveru", value=ctx.message.guild.name, inline=True)
-			embed.add_field(name="ID", value=ctx.message.guild.id, inline=True)
-			embed.add_field(name="Počet členů", value=ctx.message.guild.member_count, inline=True)
-			embed.add_field(name="Z toho boti", value=findbots, inline=True)
-			embed.add_field(name="Majitel", value=ctx.message.guild.owner, inline=True)
-			embed.add_field(name="Region", value=ctx.message.guild.region, inline=True)
-			embed.add_field(name="Vytvořeno", value= ctx.message.guild.created_at, inline=True)
-		await ctx.channel.send(content=f"Něco málo o  **{ctx.message.guild.name}**", embed=embed)
-	
-	async def randomimageapi(self, ctx, url, endpoint):
-		try:
-			r = requests.get(url)
-			r=r.json()
-		except json.JSONDecodeError:
-			return await ctx.channel.send("Chybička se vloudila")
-		await ctx.channel.send(r[endpoint])
-	
-	@commands.command(pass_context = True, aliases = ["kocka","kotatko","catto"])
-	@commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
-	async def cat(self, ctx):
-		""" Posts a random cat """
-		await self.randomimageapi(ctx, 'https://nekos.life/api/v2/img/meow', 'url')
-
-	@commands.command(pass_context = True, aliases = ["pes","pejsek","stenatko","doggo"])
-	@commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
-	async def dog(self, ctx):
-		""" Posts a random dog """
-		await self.randomimageapi(ctx, 'https://random.dog/woof.json', 'url')
-
-	@commands.command(pass_context = True, aliases = ["kachna","kachnatko"])
-	@commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
-	async def duck(self, ctx):
-		""" Posts a random duck """
-		await self.randomimageapi(ctx, 'https://random-d.uk/api/v1/random', 'url')
-	@commands.command(pass_context=True)
-	async def dong(self, ctx):
-		with open("dongs.txt",'r') as file:
-			links = json.load(file)
-			dong = random.choice(links)
-			dong = dong.strip("'")
-			await ctx.channel.send(dong)
-	
-	@commands.command(pass_context=True,aliases=["nahodnecislo","nahodne","random"])
-	@commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
-	async def randint(self,ctx,top):
-		try:
-			top = int(top)
-		except:
-			return await ctx.channel.send("S číslem je nějaká chyba...")
-		if top>100000:
-			return await ctx.channel.send("Horní limit je příliš velký, maximum je 100 000")
-		chosen = random.randrange(0,top)
-		await ctx.channel.send(f"`{chosen}`")
-
-	@commands.command(pass_context = True,aliases=["pero","ukazmipero","dick","ukažmipéro"])
-	@commands.cooldown(rate=1, per=2, type=commands.BucketType.user)
-	async def penis(self,ctx,user:discord.Member = None):
-		if user is None:
-			user = ctx.message.author
-		e=discord.Embed(colour=random.randint(0, 0xFFFFFF))
-		e.set_author(name="Pindik")
-		e.add_field(name=f"Tímto se může chlubit {str(user)[:-5]}:",value="8"+'='*random.randrange(0,10)+"D")
-		await ctx.channel.send(embed=e)
-	
-	@commands.command(pass_context= True,aliases=["detektor","lez","lež"])
-	@commands.cooldown(rate=1, per=3, type=commands.BucketType.user)
-	async def detektorlzi(self,ctx,user:discord.Member = None):
-		prvni = False
-		if user is None:
-			user = ctx.message.author
-			prvni = True
-		possible_prvni = ["Lžeš","Podle mě lžeš","Pravda zní jinak...","Ty jsi nikdy nemluvil nic jiného než pravdu :slight_smile:","Máš pravdu","Ačkoliv to nerad uznávám, máš pravdu"]
-		possbile_ostatni = [f"{user.display_name} lže",f"{user.display_name} lže, jako když tiskne",f"{ctx.message.author.display_name} má pravdu, {user.display_name} nikoliv",f"{user.display_name} má pravdu, {ctx.message.author.display_name} nikoliv",f"{user.display_name} vždy mluvil pravdu"]
-		if prvni:
-			msg = random.choice(possible_prvni)
-		else:
-			msg = random.choice(possbile_ostatni)
-		await ctx.channel.send(f"`{msg}`")
-
-	@commands.command(pass_context= True,aliases=["láska","<3","luv"])
-	@commands.cooldown(rate=1, per=3, type=commands.BucketType.user)
-	async def love(self,ctx,fuser:discord.Member=None,suser:discord.Member=None):
-		if fuser is None and suser is None:
-			return await ctx.channel.send("Nikoho jsi neoznačil!")
-		elif suser is None:
-			suser = ctx.message.author
-		url = "https://www.lovecalculator.com/love.php?"
-		fuser_was,suser_was=fuser.display_name,suser.display_name
-		fuser,suser = unidecode(fuser.display_name.replace(" ","+")),unidecode(suser.display_name.replace(" ","+"))
-		url = url+f"name1={fuser}&name2={suser}"
-		r = urllib.request.urlopen(url,cafile=certifi.where())
-		soup = BeautifulSoup(r,'html.parser')
-		result = soup.find("div", {"class":"result__score"}).text.strip()
-		await ctx.channel.send(f"{fuser_was} a {suser_was} spolu mají šanci `{result}`:heart:")
-	
-	@commands.group(pass_context= True,aliases=["svátek"])
-	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
-	async def svatek(self,ctx):
-		if ctx.invoked_subcommand is None:
-			r = requests.get("https://api.abalin.net/get/today").json()
-			svatek_cz = r["data"]["name_cz"]
-			svatek_sk = r["data"]["name_sk"]
-			await ctx.channel.send(f"Dnes má svátek `{svatek_cz}` a na Slovensku `{svatek_sk}` :ribbon:")
-			
-	@svatek.command(aliases=["zítra"])
-	async def zitra(self, ctx):
-		r = requests.get("https://api.abalin.net/get/tomorrow").json()
-		svatek_cz = r["data"]["name_cz"]
-		svatek_sk = r["data"]["name_sk"]
-		await ctx.channel.send(f"Zítra má svátek `{svatek_cz}` a na Slovensku `{svatek_sk}`:ribbon:")
-	
-	@commands.group(pass_context= True,aliases=["vahahovna"])
-	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
-	async def hovno(self,ctx,user:discord.Member=None):
-		if user == None:
-			user = ctx.message.author
-		e=discord.Embed(colour=random.randint(0, 0xFFFFFF))
-		e.set_author(name="Hovínko")
-		e.add_field(name=f"{str(user)[:-5]} má hovno o hmotnosti úctyhodných",value=str(random.randrange(0,2000))+" gramů")
-		await ctx.channel.send(embed=e)
+    @commands.command()
+    async def vs(self, ctx, h: int):
+        r = ""
+        n = "\n"
+        for i in range(1, h):
+            r += (h - i) * " " + i * "*" + (i - 1) * "*"+"\n"
+        r += (h - 1) * " " + "|"
+        await ctx.send(f"""```
+        {n}{r}
+        ```""")
 
 def setup(bot):
-	bot.add_cog(Fun(bot))
+    bot.add_cog(fun(bot))
--- cogs/math.py
+++ cogs/math.py
@@ -1,52 +1,20 @@
-import os
-import re
-import math
-import asyncio
 import discord
-import numpy as np
-import matplotlib.pyplot as plt
+import math
 from discord.ext import commands
+from discord.ext.commands import bot
+class math(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-class TimedOutExc(Exception):
-	pass
-class Math:
-	def __init__(self,bot):
+    @commands.command()
+    async def math(self, ctx, *, expression: str):
+        calculation = eval(expression)
+        em = discord.Embed(color=discord.Color.green())
+        em.set_author(name=ctx.message.author.name, icon_url=ctx.message.author.avatar_url)
+        em.add_field(name="Math:", value=f" {str(expression)} ", inline=False)
+        em.add_field(name="Answer:", value=f" {str(calculation)} ", inline=False)
+        await ctx.channel.send(embed=em)
 
-		self.bot = bot
-	async def getgraph(self,ctx,args):
-		x=np.array(np.linspace(-50,50,1000))
-		var = {'x':x,'sin':np.sin,'cos':np.cos,'sqrt':np.sqrt,'log':np.log,'abs':np.abs,'pow':np.power}
-		if "**" in args:
-			return await self.bot.send_message(ctx.message.channel,"Neplatný ** operátor, použij pow()")
-		if "x" not in args:
-			args= "x-x+"+args
-		try:
-			plt.plot(x, eval(args,var),"r-")
-		except FloatingPointError:
-			return await self.bot.send_message(ctx.message.channel,"Výpočet trval příliš dlouho")
-		except OverflowError:
-			return await self.bot.send_message(ctx.message.channel,"Čísla jsou moc velká")
-		except NameError as e:
-			print(e)
-			return await self.bot.send_message(ctx.message.channel,"Zadal jsi neznámou, kterou opravdu neznám")
-		except SyntaxError:
-			return await self.bot.send_message(ctx.message.channel,"Někde máš překlep (možná chybějící závorka?)")
-		except ZeroDivisionError:
-			return await self.bot.send_message(ctx.message.channel,"`∞`")
-		plt.grid(True, which='both')
-		plt.axhline(y=0, color='k')
-		plt.axvline(x=0, color='k')
-		filename = str(ctx.message.server.id)+".jpg"
-		plt.savefig(filename,bbox_inches='tight')
-		plt.gcf().clear()
-		await self.bot.send_file(ctx.message.channel,filename)
-		os.remove(filename)
 
-	@commands.command(pass_context=True,aliases=["y=","graf"])
-	async def graph(self,ctx,*args):		
-		await self.bot.send_typing(ctx.message.channel)	
-		np.seterr(over="raise",divide="raise")
-		args = ' '.join(args)
-		await self.getgraph(ctx,args)
 def setup(bot):
-	bot.add_cog(Math(bot))
+    bot.add_cog(math(bot))
